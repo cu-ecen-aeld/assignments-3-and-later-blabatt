4d194371ed7a6d5ff6cd625798d2d7f58d994f5a
Blair Labatt <blabattiii@gmail.com>
2026-06-14 20:22:48 -0500

    usleep, not sleep; syscall return checking was inverted

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -17,31 +17,32 @@ void* threadfunc(void* thread_param)
     struct thread_data* thread_func_args = (struct thread_data *) thread_param;
 
     // wait
-    sleep(thread_func_args->wait_obtain);
+    usleep(thread_func_args->wait_obtain);
 
     // obtain mutex (assume already initialized via:
-    // pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;)
+    // pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
     // per p236 in LSP.
     ret = pthread_mutex_lock(thread_func_args->mutex);
-    if(!ret) {
+    if(ret) {
 	perror("mutex lock");
 	thread_func_args->thread_complete_success = false;
 	return thread_param;
     }
 
     // wait
-    sleep(thread_func_args->wait_release);
+    usleep(thread_func_args->wait_release);
 
     // release mutex as described by thread_data structure
     ret = pthread_mutex_unlock(thread_func_args->mutex);
-    if(!ret) {
+    if(ret) {
 	perror("mutex unlock");
 	thread_func_args->thread_complete_success = false;
 	return thread_param;
     }
 
     thread_func_args->thread_complete_success = true;
-    return thread_param; // question: no need to use `pthread_exit()` here instead?
+    // pthread_exit(thread_param);
+    return thread_func_args; // thread_param; // question: no need to use `pthread_exit()` here instead?
 }
 
 
@@ -53,7 +54,12 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex, int
      struct thread_data* the_thread_data = malloc(sizeof(struct thread_data));
 
      // setup mutex and wait arguments:
-     // pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // <-- is this line needed for initializing mutex?
+     // mutex = PTHREAD_MUTEX_INITIALIZER; // <-- is this line needed for initializing mutex?
+     //if (pthread_mutex_init(mutex, NULL) != 0) {
+     //    printf("Mutex initialization failed\n");
+     //    return 1;
+     //}
+
      the_thread_data->mutex = mutex;
      the_thread_data->wait_obtain  = wait_to_obtain_ms;
      the_thread_data->wait_release = wait_to_release_ms;
